4fabdbf7b03414c1311f5f6f54c55dd913e7f856
Jamie Evans <jamie@jedeisign.xyz>
2019-11-20 23:41:53 +0000

    finishing up project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,33 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // IBOutlets
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    
+    // consts - dict
     let eggTime = ["Soft": 3, "Medium": 4, "Hard": 7]
     var secondsRemaining = 60
+    // initialise increment as float
     var increment: Float = 1.0
     var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        // set hardness from button title
         let hardness = sender.currentTitle! //Soft, Medium, Hard
+        // rset default label
+        titleLabel.text = "How do you like your eggs?"
+        // set default progress percentange
         progressBar.progress = 0
+        // reset timer
         timer.invalidate()
         timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-        
+        // set to dict key:value
         secondsRemaining = eggTime[hardness]!
+        // calculate increment percentage based on secondsRemaning to be added each second to progressBar
         increment = 1 / Float(secondsRemaining)
     }
     
     @objc func updateTimer() {
         if secondsRemaining > 0 {
             print("\(secondsRemaining) seconds.")
+            // increment progressBar
             progressBar.progress += increment
             secondsRemaining -= 1
         } else {
+            // reset time
             timer.invalidate()
             titleLabel.text = "DONE!"
+            // fill progress bar
             progressBar.progress = 1.0
         }
     }
